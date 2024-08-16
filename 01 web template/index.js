d274cfbc7f3591b8970db415a4fe21647feff1c3
Josh5500 <joshuaelias2006@gmail.com>
2024-08-16 11:22:34 +0200

    first commit

---
+++ 01 web template/index.js
@@ -0,0 +1,34 @@
+console.log('p5.js er loaded')
+let pages //array med alle elementer med class = page
+
+let currentPage = 1
+
+function setup(){
+    pages = selectAll('.page')
+    
+    
+    //console.log(pages.length)
+
+}
+
+function shiftPage(num){
+if(num == "ArrowLeft"){
+    num = currentPage - 1
+}
+if(num == "ArrowRight"){
+    num = currentPage + 1
+}
+
+
+    if(isNaN(num) || num > pages.length || num == 0){
+        return
+
+    }
+    select("#page" + currentPage).removeClass('visible')
+    currentPage = num
+    select("#page" + currentPage).addClass('visible')
+}
+
+function keyPressed(){
+    shiftPage(key)
+}
