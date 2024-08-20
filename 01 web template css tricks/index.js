029c0f3c3317fbe7a7b163c576d090cbaeafc48f
Josh5500 <joshuaelias2006@gmail.com>
2024-08-20 09:25:21 +0200

    yhyhiyh3

--- 01 web template css tricks/index.js
+++ 01 web template css tricks/index.js
@@ -1,7 +1,9 @@
 console.log('p5.js er loaded')
 let pages //array med alle elementer med class = page
+let colors = ['red','green','blue','orange','cyan','grey','purple','lightblue']
 
-let currentPage = 1
+
+let currentPage = 3
 
 function setup(){
     select('#page' + currentPage).addClass('visible')
@@ -10,6 +12,13 @@ function setup(){
     
     //console.log(pages.length)
 
+    //lav en masse div'er vi kommer ind i page3
+    for(c of colors){
+        //console.log(c)
+        let div = createDiv()
+        div.style('background-color', c)
+        select('#page3').child(div)
+    }
 }
 
 function shiftPage(num){
