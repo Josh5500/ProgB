ed6b26e0d3f87eb280ee7893cc9d40990905da81
Josh5500 <joshuaelias2006@gmail.com>
2024-08-23 11:06:40 +0200

    fixed template java

--- 01_web_template_css_tricks/index.js
+++ 01_web_template_css_tricks/index.js
@@ -2,16 +2,28 @@ console.log('p5.js er loaded')
 let pages //array med alle elementer med class = page
 let colors = ['red','green','blue','orange','cyan','grey','purple','lightblue']
 
-
-let currentPage = 3
+let menuItems //array med alle menupunkterne
+let currentPage = 1
+let menuNumber = 1
 
 function setup(){
     select('#page' + currentPage).addClass('visible')
+    select('#menu' + menuNumber).addClass('active')
     pages = selectAll('.page')
+    menuItems = selectAll('.menuItem')
     
     
     //console.log(pages.length)
 
+    for(m of menuItems){
+        m.mousePressed( function(e){
+        //e.target er selve html div'en
+            console.log(e.target.id)
+            //slice -1 henter det sidste bogstav i en string
+            let nr = e.target.id.slice(-1)
+            shiftPage(nr)
+        } )
+    }
     //lav en masse div'er vi kommer ind i page3
     for(c of colors){
         //console.log(c)
@@ -34,9 +46,13 @@ if(num == "ArrowRight"){
         return
 
     }
+
     select("#page" + currentPage).removeClass('visible')
     currentPage = num
     select("#page" + currentPage).addClass('visible')
+    select("#menu" + menuNumber).removeClass('active')
+    menuNumber = num
+    select("#menu" + menuNumber).addClass('active')
 }
 
 function keyPressed(){
